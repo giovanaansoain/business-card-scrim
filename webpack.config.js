14f010ef356f63032d0e0faac2bdd5cee7c9de34
Giovana Ansoain <giovanaansoain@gmail.com>
2026-03-13 16:04:21 -0300

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "output": {
+    "filename": "[name].pack.js"
+  },
+  "resolve": {
+    "extensions": [
+      ".js",
+      ".json"
+    ],
+    "alias": {}
+  },
+  "module": {
+    "rules": []
+  },
+  "entry": {
+    "index": "./index"
+  }
+}
